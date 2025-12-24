bc440ccc6c9630b7fc0795fbdac2ef8da7f7d0d0
Jattzz <108416182+jattzz@users.noreply.github.com>
2025-12-24 09:57:01 +0000

    finished operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -18,3 +18,27 @@ console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false;
 // "" => false; "abc" = true;
+
+// *************** OPERATIONS *****************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+
+let str1 = "hello "
+let str2 = "feroshi"
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);   // 122
+console.log(1 + 2 + "2");   // 32
+
+// basically this is all according to the operation precedence in js, whilst trying to add up different type value, string is the most prefered data type for the res.
