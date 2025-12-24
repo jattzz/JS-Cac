e84d9bda25f7934e2180c6bd81d16b22cf4536c5
Jattzz <108416182+jattzz@users.noreply.github.com>
2025-12-24 10:06:13 +0000

    comparisons in datatypes

---
+++ basics/comparison.js
@@ -0,0 +1,11 @@
+// console.log("-02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null === 0);
+
+// null comparison is a bit tricky, undefined will always give false, since its undefined.
+
+// tripple equality => strict check => checks the value as well as the data type.
+
